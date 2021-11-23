e53ef0efc7ed1657dc54abbeb58d3a639556f0d5
Callum <cdffyi@gmail.com>
2021-11-24 00:40:15 +0200

    set default threads to 2

--- src/com/ikhokha/techcheck/Main.java
+++ src/com/ikhokha/techcheck/Main.java
@@ -14,7 +14,7 @@ public class Main {
 	
 	public static void main(String[] args) {
 		Map<String, Integer> results = new HashMap<String, Integer>();
-		int maxThreads = 5; //default unless provided as a --thread x param
+		int maxThreads = 2; //default unless provided as a --thread x param
 		String keywordsToFind = "ik pos,mover,shaker";
 
 		int argPos;
